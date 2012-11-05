09a36ecec776eee1435dc523087a68879180abd8
Justin Salmon <jsalmon@cern.ch>
2012-11-05 17:35:12 +0100

    Add comments

--- src/XrdLatencyTest.hh
+++ src/XrdLatencyTest.hh
@@ -41,80 +41,90 @@ public:
     int floodinterval;
 
     /**
+     * Add a host to the vector of hosts to be tested.
      * 
-     * @param host
-     * @param port
-     * @return 
+     * @param host: the hostname of the host to be added
+     * @param port: the xrootd port of the host
+     * @return
      */
     int addHost(std::string host, int port);
 
     /**
+     * Add a host to the vector of hosts to be tested.
      * 
-     * @param host "<node>[:port]"
+     * @param host: the hostname/port of the host to be added in the
+     *              form: "<hostname>[:port]"
      * @return 
      */
     int addHost(std::string host);
 
     /**
-     * 
+     * Print out the current list of hosts.
      */
     void printHosts();
 
     /**
+     * Remove a host from this test.
      * 
-     * @param host
-     * @param port
+     * @param host: the hostname of the host to be removed
+     * @param port: the xrootd port that this host uses
      * @return 
      */
     int removeHost(std::string host, int port);
 
     /**
+     * Remove a host from this test.
      * 
-     * @param host "<node>[:port]"
+     * @param host: the hostname/port of the host to be removed in the
+     *              form: "<hostname>[:port]"
      * @return 
      */
     int removeHost(std::string host);
 
     /**
+     * Set the path to stat on the remote box. Defaults to "/".
      * 
-     * @param path
+     * @param path: the local path on the remote box.
      * @return 
      */
     int setStatPath(std::string path = "/");
 
     /**
+     * Set the interval between single stat requests during operation.
      * 
-     * @param seconds
+     * @param seconds: the number of seconds between stats
      * @return 
      */
     int setStatInterval(size_t seconds);
 
     /**
+     * Set the interval between stat floods during operation.
      * 
-     * @param seconds
+     * @param seconds: the number of seconds between floods
      * @return 
      */
     int setFloodInterval(size_t seconds);
 
     /**
-     * Print the latest results to out and errors to err
+     * Print the latest results to out and errors to err.
      * 
-     * @param out
-     * @param err
+     * @param out: successful results
+     * @param err: error results
      */
     void PrintOut(std::string &out, std::string &err);
 
     /**
-     * Start the thread doing the measurements optionally with stat or flood option 
+     * Start the thread doing the measurements - optionally with stat or flood 
+     * options 
      * 
-     * @param do_stat
-     * @param do_flood
+     * @param do_stat: whether or not to use stat mode.
+     * @param do_flood: whether or not to use flood mode.
      * @return 
      */
     bool Start(bool do_stat, bool do_flood);
 
     /**
-     * Stop the thread doing the measurements
+     * Stop the thread doing the measurements.
      *
      * @return 
      */
